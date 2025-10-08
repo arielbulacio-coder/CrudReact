4954e6042c8c91ebe00900aeb77de6dcf80f95c0
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2025-10-08 20:26:36 -0300

    Etapa 1 terminada

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import 'font-awesome/css/font-awesome.min.css'; 
+// import 'font-awesome/css/font-awesome.min.css'; 
 
 const Footer = () => {
   return (
